8e001b37f0daf8a84d256a06033abc04d837363d
mythili2392 <mythilivarad06@gmail.com>
2026-03-30 10:53:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "myec2" {
   ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Kiruma-EC2"
