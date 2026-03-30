08674df53aa68d2fee67ce1e2a748e47fde2529b
mythili2392 <mythilivarad06@gmail.com>
2026-03-30 11:22:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,86 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "myec2" {
-  ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t3.micro"
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  tags = {
-    Name = "Kiruma-EC2"
+# Subnet
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Security Group
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS Cluster
+resource "aws_ecs_cluster" "main" {
+  name = "kiruma-cluster"
+}
+
+# Task Definition
+resource "aws_ecs_task_definition" "app" {
+  family                   = "nginx-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = "nginx"
+      image = "nginx"
+      portMappings = [{
+        containerPort = 80
+      }]
+    }
+  ])
+}
+
+# ECS Service
+resource "aws_ecs_service" "service" {
+  name            = "nginx-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = [aws_subnet.subnet.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.sg.id]
   }
 }
