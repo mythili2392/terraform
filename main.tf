feb5126f0abdf90ac3f52a50ad926dae507b7f9d
mythili2392 <mythilivarad06@gmail.com>
2026-03-30 12:08:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,44 +2,40 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# VPC
+# ---------------- VPC ----------------
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Subnet (PUBLIC)
+# ---------------- SUBNET ----------------
 resource "aws_subnet" "subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
-  map_public_ip_on_launch = true   # 🔥 IMPORTANT FIX
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
-# Internet Gateway
+# ---------------- INTERNET ----------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-# Route Table
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.main.id
 }
 
-# Route to Internet
 resource "aws_route" "route" {
   route_table_id         = aws_route_table.rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-# Associate Route Table
 resource "aws_route_table_association" "rta" {
   subnet_id      = aws_subnet.subnet.id
   route_table_id = aws_route_table.rt.id
 }
 
-# Security Group
-resource "aws_security_group" "sg" {
+# ---------------- SECURITY GROUP (ECS) ----------------
+resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
@@ -57,12 +53,31 @@ resource "aws_security_group" "sg" {
   }
 }
 
-# ECS Cluster
+# ---------------- SECURITY GROUP (ALB) ----------------
+resource "aws_security_group" "lb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ---------------- ECS CLUSTER ----------------
 resource "aws_ecs_cluster" "main" {
   name = "kiruma-cluster"
 }
 
-# Task Definition
+# ---------------- TASK DEFINITION ----------------
 resource "aws_ecs_task_definition" "app" {
   family                   = "nginx-task"
   requires_compatibilities = ["FARGATE"]
@@ -81,7 +96,37 @@ resource "aws_ecs_task_definition" "app" {
   ])
 }
 
-# ECS Service
+# ---------------- LOAD BALANCER ----------------
+resource "aws_lb" "alb" {
+  name               = "kiruma-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.subnet.id]
+}
+
+# ---------------- TARGET GROUP ----------------
+resource "aws_lb_target_group" "tg" {
+  name        = "kiruma-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+}
+
+# ---------------- LISTENER ----------------
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+# ---------------- ECS SERVICE ----------------
 resource "aws_ecs_service" "service" {
   name            = "nginx-service"
   cluster         = aws_ecs_cluster.main.id
@@ -91,7 +136,15 @@ resource "aws_ecs_service" "service" {
 
   network_configuration {
     subnets          = [aws_subnet.subnet.id]
-    assign_public_ip = true   # 🔥 IMPORTANT
-    security_groups  = [aws_security_group.sg.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_sg.id]
   }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.listener]
 }
